d5479e815a110ca95f71a5fc7e24d1db33f4a78d
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2020-12-07 09:31:10 +0200

    create question component

--- open_psych_reseaech.react/src/App.tsx
+++ open_psych_reseaech.react/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Question from "./components/Survey/question"
 
 function App() {
   return (
     <div className="App">
+      <Question prompt="Q" options={["1","2"]} required={false}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
